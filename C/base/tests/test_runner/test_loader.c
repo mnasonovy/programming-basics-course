823cd7c7800ef3e966b6caaead8345a80ebfdafc
Mikhail <mnasonovy@gmail.com>
2025-11-07 00:43:17 +0400

    add 06.11.25_practic

--- C/base/tests/test_runner/test_loader.c
+++ C/base/tests/test_runner/test_loader.c
@@ -14,26 +14,29 @@ typedef struct {
 int load_questions(const char *filename, Question *questions) {
     FILE *file = fopen(filename, "r");
     if (!file) {
-        perror("\033[1;31mОшибка при открытии файла\033[0m");
+        fprintf(stderr, "\033[1;31mОшибка при открытии файла: %s\033[0m\n", filename);
         return 0;
     }
 
     char line[MAX_LINE];
     int count = 0;
-    while (fgets(line, sizeof(line), file)) {
+    while (fgets(line, sizeof(line), file) && count < MAX_QUESTIONS) {
         line[strcspn(line, "\n")] = 0;
         if (strlen(line) < 5) continue;
 
         char *token = strtok(line, ";");
         if (!token) continue;
-        strncpy(questions[count].question, token, sizeof(questions[count].question));
+        strncpy(questions[count].question, token, sizeof(questions[count].question) - 1);
+        questions[count].question[sizeof(questions[count].question) - 1] = '\0';
 
         for (int i = 0; i < 4; i++) {
             token = strtok(NULL, ";");
-            if (token)
-                strncpy(questions[count].options[i], token, sizeof(questions[count].options[i]));
-            else
-                strcpy(questions[count].options[i], "");
+            if (token) {
+                strncpy(questions[count].options[i], token, sizeof(questions[count].options[i]) - 1);
+                questions[count].options[i][sizeof(questions[count].options[i]) - 1] = '\0';
+            } else {
+                questions[count].options[i][0] = '\0';
+            }
         }
 
         token = strtok(NULL, ";");
